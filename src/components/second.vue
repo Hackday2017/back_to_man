5aaf9c678b09ab8228850d1fa360b3b9726e006e
Cruyun <1045671796@qq.com>
2017-11-19 12:48:54 +0800

    fix dialog

--- src/components/second.vue
+++ src/components/second.vue
@@ -9,43 +9,39 @@
       </div>
       <div v-else-if="this.word == 1">
         <div class="name">【小猫】</div>
-        <div class="word">我...我怎么变成了猫？！！这到底是怎么回事？？</div>
+        <div class="word">我本来是人，醒来不知道为什么变成了猫，我不记得发生了什么。
+       你是谁？你为什么会在这里？</div>
       </div>
       <div v-else-if="this.word == 2">
-        <div class="name">【小猫】</div>
-        <div class="word">我本来是人，醒来不知道为什么变成了猫，我不记得发生了什么。 你是谁？你为什么会在这里？
-        </div>
-      </div>
-      <div v-else-if="this.word == 3">
         <div class="name">【病人】</div>
         <div class="word">真是神奇...这里是精神病院，我被当成病人抓到这里了，其实我并没有什么精神病。</div>
       </div>
-      <div v-else-if="this.word == 4">
+      <div v-else-if="this.word == 3">
         <div class="name">【小猫】</div>
         <div class="word">你没有病？那你为什么会被抓到这里</div>
       </div>
-      <div v-else-if="this.word == 5">
+      <div v-else-if="this.word == 4">
         <div class="name">【病人】</div>
         <div class="word">我本来是这家医院的医生。 他们在研发新药，用人体做活体实验，我发现了他们的机密上报给公关机构，却被医院用「妄想症」诊断书驳回。
         </div>
       </div>
-      <div v-else-if="this.word == 6">
+      <div v-else-if="this.word == 5">
         <div class="name">【小猫】</div>
         <div class="word">他们在研制什么药？</div>
       </div>
-      <div v-else-if="this.word == 7">
+      <div v-else-if="this.word == 6">
         <div class="name">【病人】</div>
         <div class="word">我也不太清楚</div>
       </div>
-      <div v-else-if="this.word == 8">
+      <div v-else-if="this.word == 7">
         <div class="name">【小猫】</div>
         <div class="word">这会是我变成猫的原因吗...?</div>
       </div>
-      <div v-else-if="this.word == 9">
+      <div v-else-if="this.word == 8">
         <div class="name">【病人】</div>
         <div class="word">我想是的。希望你相信我的话。</div>
       </div>
-      <div v-else-if="this.word == 10">
+      <div v-else-if="this.word == 9">
         <div class="choice">
           <div class="arrow inline_block"></div>
           <div class="yes inline_block" v-on:click="next">【相信】</div>
@@ -55,24 +51,24 @@
           <div class="no inline_block" v-on:click="not_believe">【不相信】</div>
         </div>
       </div>
-      <div v-else-if="this.word == 11">
+      <div v-else-if="this.word == 10">
         <div class="name">【小猫】</div>
         <div class="word">我相信你</div>
       </div>
-      <div v-else-if="this.word == 12">
+      <div v-else-if="this.word == 11">
         <div class="name">【病人】</div>
         <div class="word">太好了。他们有研发成功的药，那些药藏在制药基地，用箱子锁了起来。
             被抓走前我把钥匙偷走了。希望能给你带来帮助。</div>
       </div>
-      <div v-else-if="this.word == 13">
+      <div v-else-if="this.word == 12">
         <div>【获得钥匙】</div>
         <img src="../pictures/key.png" class="key">
       </div>
-      <div v-else-if="this.word == 14">
+      <div v-else-if="this.word == 13">
         <div class="name">【小猫】</div>
         <div class="word">我想你病的不轻</div>
       </div>
-      <div class="next" v-on:click="this.next" v-if="this.word != 10 && this.word != 15"></div>
+      <div class="next" v-on:click="this.next" v-if="this.word != 9 && this.word != 14"></div>
     </div>
   </div>
 </template>
@@ -93,10 +89,13 @@
       },
       next() {
         this.word++;
-        if (this.word == 13) {
+        if (this.word == 12) {
           this.key = true;
+        }
+          else if (this.word == 13) {
           this.he()
-        } else if (this.word == 15) {
+          }
+        else if (this.word == 14) {
           this.be()
         }
       },
@@ -143,7 +142,7 @@
     cursor: pointer;
   }
   .key {
-    margin-top: 30px;
+    margin-top: 10px;
     margin-left: 30px;
   }
 </style>
